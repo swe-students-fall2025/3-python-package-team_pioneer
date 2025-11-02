762b544be3689c00e9e6a6453576a43e7d96d5ca
player1notfound <123220161+player1notfound@users.noreply.github.com>
2025-11-02 17:02:02 -0500

    add

--- src/demo.py
+++ src/demo.py
@@ -5,6 +5,5 @@ def main():
     pet = create_pet("Mochi", "capybara", mood="happy", hunger=20, energy=80)
     print(status(pet, color=True, verbose=True, ascii_art=True))  
  
-
 if __name__=="__main__":
     main()
